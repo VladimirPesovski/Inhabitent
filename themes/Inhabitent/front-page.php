988a10c51fa534b4cb8caeb8066bdc805a63f708
Vladimir Pesovski <vladimirpesovski@Vladimirs-MacBook-Pro.local>
2019-11-06 10:14:22 -0500

    fix: another initial commit

---
+++ themes/Inhabitent/front-page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<h1><i class="fab fa-angrycreative"></i></h1>
+
+<?php if( have_posts() ) :
+//The WordPress Loop: loads post content 
+    while( have_posts() ) :
+        the_post(); ?>
+
+    <?php endwhile;?>
+
+    
+
+    <?php the_posts_navigation();?>
+
+<?php else : ?>
+        <p>No posts found</p>
+<?php endif;?>
+
+    
+<?php get_footer();?>
